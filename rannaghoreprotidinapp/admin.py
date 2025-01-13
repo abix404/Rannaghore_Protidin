0c7b9c1448792b1b8f836e55d71da07a569cde26
Abid Muhammad <abidmuhammad206@gmail.com>
2025-01-14 04:00:18 +0600

    update

--- rannaghoreprotidinapp/admin.py
+++ rannaghoreprotidinapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Cart, Order
 
 # Register your models here.
+
+admin.site.register([Product, Cart, Order])
--- rannaghoreprotidinapp/models.py
+++ rannaghoreprotidinapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    first_name=models.CharField(max_length=15,blank=False,null=False)
+    last_name=models.CharField(max_length=15,blank=False,null=False)
+    mobile_no=models.IntegerField(blank=False,null=False)
+    email=models.EmailField()
+    bio=models.CharField(max_length=200,null=True,blank=True)
+
+class Product(models.Model):
+    name=models.CharField(max_length=100,blank=False,null=False)
+    price=models.IntegerField(blank=True,null=True)
+    categories=models.CharField(max_length=50,blank=True,null=True)
+    short_description=models.CharField(max_length=100, blank=False,null=False)
+    brief_description=models.CharField(max_length=500)
+    brand=models.CharField(max_length=20)
+    sku=models.IntegerField(unique=True)
+def __str__(self):
+    return self.name
+
+class Cart(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE,blank=True,null=True)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    ctrate_date=models.DateTimeField(auto_now_add=True,auto_now=False)
+    order_id=models.IntegerField(unique=True)
+
+
+
+
+
+
+
